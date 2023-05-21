5d605be772fd4d15585603ca378e4da3eef2c560
HerlanBenavides <94395211+7HnKt2jP8QzE@users.noreply.github.com>
2023-05-21 00:48:58 -0600

    2

--- script.js
+++ script.js
@@ -1,18 +1,1 @@
-function submitPassword() {
-    var passwordInput = document.getElementById('password-input');
-    var errorMessage = document.getElementById('error-message');
-  
-    var password = passwordInput.value;
-    // Aquí puedes cambiar la contraseña a tu preferencia
-    var correctPassword = 'PostulacionAcademica';
-  
-    if (password === correctPassword) {
-      // Mostrar el contenido principal si la contraseña es correcta
-      document.getElementById('password-form').style.display = 'none';
-      document.getElementById('main-content').style.display = 'block';
-    } else {
-      // Mostrar mensaje de error si la contraseña es incorrecta
-      errorMessage.textContent = 'Contraseña incorrecta';
-    }
-  }
-  
+function submitPassword() { var passwordInput = document.getElementById('password-input'); var errorMessage = document.getElementById('error-message'); var password = passwordInput.value; var correctPassword = 'PostulacionAcademica'; if (password === correctPassword) { document.getElementById('password-form').style.display = 'none'; document.getElementById('main-content').style.display = 'block'; } else { errorMessage.textContent = 'Contraseña incorrecta'; } }
